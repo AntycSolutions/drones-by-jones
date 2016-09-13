cf4c61732f6dfb579c0e2450f48dcf95955fb965
Solanar <and_555@hotmail.com>
2016-09-13 14:28:30 -0600

    add configs folder to git, update activate script to be multiplatform

--- activate.sh
+++ activate.sh
@@ -1,2 +1,12 @@
-source ../venv_dbj/Scripts/activate
+
+platform=`python -c "import platform; print(platform.system())"`
+
+if [[ "$platform" == 'Linux' ]]; then
+    source ../venv_dbj/bin/activate
+elif [[ "$platform" == 'Windows' ]]; then
+    source ../venv_dbj/Scripts/activate
+else
+    echo "Unsupported platform: $platform"
+fi
+
 export DJANGO_SETTINGS_MODULE=dbj.settings
